7dc88616ada1d315cdbaea162534c0c9f665a348
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-10-17 12:03:37 +0300

    edited insert methods

--- linked_list.py
+++ linked_list.py
@@ -2,33 +2,127 @@
 # pylint: disable=C0115
 # pylint: disable=C0116
 # pylint: disable=C0301
+# pylint: disable=C0304
+# pylint: disable=C0103
+# pylint: disable=C0303
 class Node:
-    def __init__(self,data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
 
+# first= Node(24)
+# second= Node(7)
+# third=Node(9)
+
+# first.next =second
+# second.next=third
+
+# current = first
+
+# while current is not None:
+#     print(current.data,end="-->")
+#     current=current.next
+
+
 class LinkedList:
-    def __init__(self,node:Node):
-        self.node= node
-        self.head= None
+    def __init__(self):
+        self.head = None
+    # head = None
 
     def insert_at_beginning(self, value):
-        pass
+        new_node= Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = new_node
+            current.next = self.head
+            self.head = new_node
 
     def insert_at_end(self, value):
-        pass
+        new_node= Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+
+            current.next = new_node
 
     def delete_value(self, value):
-        pass
+        previous = None
+        current = self.head
+        if self.head is None:
+            return
 
-    def search(self, value):
-        pass
+        elif current.value == value:
+            self.head = current.next
+            current.next = None
+            print(f"item {value} deleted")
+            return current
+
+        else:
+            while current is not None:
+                if current.value == value:
+                    break
+                previous = current
+                current = current.next
+
+            previous.next = current.next
+            current.next = None
+            print(f"item {value} deleted")
+        self.display()
+
+    def search(self, node):
+        if self.head is None:
+            print("List is empty")
+        else:
+            current = self.head
+            while current is not None:
+                if current == node.value:
+                    print(current.value==node.value)
+                    current = current.next
+            
+
+
 
     def display(self):
-        pass
+        if self.head is None:
+            print("List is empty")
+        else:
+            current = self.head
+            while current is not None:
+                print(current.value, end=" --> ")
+                current = current.next
+            print("None")
 
 
-linked_list= LinkedList(10)
-print(linked_list.node,end="-->")
-# linked_list.head = 
+linked_list = LinkedList()
+
+linked_list.insert_at_end(15)
+# linked_list.insert_at_end(Node(47))
+# linked_list.insert_at_end(Node(94))
+# linked_list.insert_at_end(Node(51))
+# linked_list.insert_at_end(Node(3))
+
+# linked_list.display()
+
+linked_list.insert_at_beginning(25)
+linked_list.insert_at_beginning(64)
+
+linked_list.display()
+
+
+linked_list.delete_value(64)
+# linked_list.delete_value(3)
+linked_list.delete_value(25)
+# linked_list.delete_value(94)
+# linked_list.delete_value(15)
+# linked_list.delete_value(47)
+# linked_list.delete_value(51)
+# linked_list.delete_value(51)
+# linked_list.delete_value(22)
+
+
+linked_list.display()
